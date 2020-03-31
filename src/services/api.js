549eaec6c5111577ce8819f8a621100ab121f7cd
Felipe Santiago <you@example.com>
2020-03-31 02:53:00 -0300

    Change url api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://app-be-the-hero-api.herokuapp.com/',
+  baseURL: 'https://app-be-the-hero-api.herokuapp.com',
 });
 
 export default api;
